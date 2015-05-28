3a443da8ec1858c09824b26df4726c00148a8de5
Alex. C <alex-clarke13@live.co.uk>
2015-05-28 15:52:58 +0100

    Player hit detection / Enemy Implementation
    Enemy Spawning
    Enemy Movement (towards playing position)
    Enemy  Animation (face towards player - review)

--- src/Main.lua
+++ src/Main.lua
@@ -62,8 +62,8 @@
 		-- Firing
 			-- Hit detection on walls / blocks
 		-- Swapping
-			-- Switch position with selected block
-			-- Mouse control
+			-- Switch position with selected block [Done]
+			-- Mouse control [Done]
 
 	-- Health / Score / Magic Display
 		-- Use provided sprites + text
@@ -106,18 +106,30 @@ love.keyboard.setKeyRepeat(false)
 
 	--Code to load spritesheet from website through api
 
-
 	--Individual sprites - 16x16 
 	spritesheet = love.graphics.newImage("spritesheet.png")
 
-	--Player
-	playersprite = love.graphics.newQuad(0,0,16,16, spritesheet:getDimensions())
 
 	--Enemy Type - 1
-	enemy1sprite = love.graphics.newQuad(16,16,16,16, spritesheet:getDimensions())
+	enemyType1 = {
+					upsprites = extractSprites(16,18,16,16),
+					downsprites = extractSprites(10,12,16,16),
+					leftsprites = extractSprites(13,15,16,16),
+					rightsprites = extractSprites(13,15,16,16),
+					movespeed = 20,
+					health = 1
+				}
 
 	--Enemy Type - 2
-	enemy2sprite = love.graphics.newQuad(32,32,16,16, spritesheet:getDimensions())
+	enemyType2 = {
+					upsprites = extractSprites(25,27,16,16),
+					downsprites = extractSprites(19,21,16,16),
+					leftsprites = extractSprites(22,24,16,16),
+					rightsprites = extractSprites(22,24,16,16),
+					movespeed = 20,
+					health = 2
+				}
+
 
 	testsprites = extractSprites(8,10,16,16)
 
@@ -125,8 +137,6 @@ love.keyboard.setKeyRepeat(false)
 	Player = {
 				xpos = 50,
 				ypos = 50,
-				cx = 42,
-				cy = 42,
 				health = 3,
 				xspeed = 0,
 				yspeed = 0,
@@ -143,13 +153,14 @@ love.keyboard.setKeyRepeat(false)
 			 	shotsprite = extractSprites(29,29,16,16),
 			 	spriteindex = 1,
 			 	flip = 1,
-			 	moving = {up = false, down = false, left = false, up = false}
+			 	moving = {up = false, down = false, left = false, up = false},
+			 	rad = 4
 			 }
 
 	--Arena
 	Arena = {wallsprites = extractSprites(49,56,16,16),
 			 floorsprite = extractSprites(40,40,16,16),
-			 obstacles = createObstacles()
+			 obstacles = createObstacles(25)
 			}	
 
 	--Interface
@@ -163,6 +174,11 @@ love.keyboard.setKeyRepeat(false)
 	--Projectiles
 	Projectiles = {}
 
+	--Enemies
+	EnemyTable = {
+		maxEnemies = 1000,
+		Enemies = {}
+	}
 
 	--Timer
 	min_dt = 1/60
@@ -177,15 +193,17 @@ function love.update(dt)
 
 	next_time = next_time + min_dt
 
-	Player.xpos = Player.xpos + (dt * Player.xspeed)
-	Player.ypos = Player.ypos + (dt * Player.yspeed)
-	Player.cx = Player.cx + (dt * Player.xspeed)
-	Player.cy = Player.cy + (dt * Player.yspeed)
 
+	if(not playerHitDetection(dt)) then
+		Player.xpos = Player.xpos + (dt * Player.xspeed)
+		Player.ypos = Player.ypos + (dt * Player.yspeed)
+	end
 	animatePlayer()
 
 	updateProjectiles(dt)
+	projectileHitDetection()
 	cleanupProjectiles(dt)
+	updateEnemies(dt)
 
 end
 
@@ -252,79 +270,18 @@ function love.mousereleased(x,y, button)
 
 	--Fire
 	if button == 'l' then
-
-		if(#Projectiles < Player.maxshots) then
-
-			--Treat as triangle
-			xlen = ((x - Player.cx)) -- X (Adjacent) 
-			ylen = ((y - Player.cy)) -- Y (Opposite)
-
-			r = math.sqrt((xlen * xlen) + (ylen * ylen)) -- R (Hypotenuse)
-
-			xratio = xlen / r -- Ratio of x to r (Sin[theta]), value to increase x coordinate by each dt
-			yratio = ylen / r -- Ratio of y to r (Cos[theta]), value to increase y coordinate by each dt
-
-			xd = true
-			yd = true
-
-			if(xlen < 0) then
-				xd = false
-			end
-
-			if(ylen < 0) then
-				yd = false
-			end
-
-
-			shot = {xpos = Player.cx, ypos = Player.cy,
-					targetx = x, targety = y, 
-					sprite = Player.shotsprite[1], 
-					xinc = xratio, yinc = yratio, 
-					xdist = xd, ydist = yd}
-
-			table.insert(Projectiles, shot)
-
-		end
+		createBullet(x,y)
 	end
 
 	--Swap
 	if button == 'r' then
-
-
-		-- Check if any blocks are selected
-		for i = 1, #Arena.obstacles, 1 do
-
-			--Four corners of block
-			blockx1 = Arena.obstacles[i].x
-			blockx2 = blockx1 + 16
-			blocky1 = Arena.obstacles[i].y
-			blocky2 = blocky1 + 16
-
-
-
-			if((x >= blockx1 and x <= blockx2) and (y >= blocky1 and y <= blocky2) ) then
-
-				Arena.obstacles[i].x = Player.xpos
-				Arena.obstacles[i].y = Player.ypos
-
-				Player.xpos = blockx1
-				Player.ypos = blocky1
-
-				return	
-			end
-
-		end
+		swapBlock(x,y)
 	end
-
-
-
 
 
 end
 
-
 function love.draw()
-
 
 	love.graphics.print(#obstacles, 50,50)
 
@@ -336,7 +293,9 @@ function love.draw()
 	love.graphics.draw(spritesheet, Player.currentsprites[Player.spriteindex], Player.xpos, Player.ypos, 0, Player.flip, 1, 8, 8)
 
 	drawProjectiles();
+	love.graphics.circle( "fill", Player.xpos, Player.ypos, Player.rad, 5)
 
+	drawEnemies()
 
    local cur_time = love.timer.getTime() 
    if next_time <= cur_time then
@@ -376,7 +335,6 @@ function drawArenaWalls()
 	lftcolx = 0
 	rhtcolx = 784
 
-
 	-- Top/Bottom Rows
 	for row = 0, 49, 1 do
 		love.graphics.draw(spritesheet, Arena.wallsprites[6], row*16, toprowy)
@@ -413,12 +371,13 @@ function drawArenaObstacles()
 
 	for i = 1, #Arena.obstacles, 1 do
 		love.graphics.draw(spritesheet, Arena.obstacles[i].sprite, Arena.obstacles[i].x, Arena.obstacles[i].y)
+		love.graphics.circle( "fill", Arena.obstacles[i].cx, Arena.obstacles[i].cy, Arena.obstacles[i].rad, 5)
 	end 
 
 end
 
 
-function createObstacles()
+function createObstacles(obs)
 
 	-- Arena Obstacles - Rows 5/6, skip last entry - used for floor tile
 	obstaclesprites = extractSprites(33,39,16,16)
@@ -431,7 +390,7 @@ function createObstacles()
 
 	--Create obstacles
 	obstacles = {}
-	for i = 1, 25, 1 do
+	for i = 1, obs, 1 do
 
 		--Create x/y coordinates
 		xpos = math.random(16, 768)
@@ -440,7 +399,7 @@ function createObstacles()
 		--Randomise tile
 		tileind = math.random(1, #obstaclesprites)
 
-		table.insert(obstacles, {x = xpos, y = ypos, sprite = obstaclesprites[tileind]})
+		table.insert(obstacles, {x = xpos, y = ypos, sprite = obstaclesprites[tileind], rad = 8, cx = xpos + 8, cy = ypos + 8})
 
 	end 
 
@@ -486,6 +445,8 @@ function drawProjectiles()
 
 	for i = 1, #Projectiles, 1 do
 		love.graphics.draw(spritesheet, Projectiles[i].sprite, Projectiles[i].xpos, Projectiles[i].ypos)
+		love.graphics.circle( "fill", Projectiles[i].cx, Projectiles[i].cy, Projectiles[i].rad, 5)
+
 	end
 
 end 
@@ -498,10 +459,67 @@ function updateProjectiles(dt)
 			Projectiles[i].ypos = Projectiles[i].ypos + (dt * Player.firespeed * Projectiles[i].yinc)
 			Projectiles[i].xpos = Projectiles[i].xpos + (dt * Player.firespeed * Projectiles[i].xinc)
 
+			Projectiles[i].cy = Projectiles[i].cy + (dt * Player.firespeed * Projectiles[i].yinc)
+			Projectiles[i].cx = Projectiles[i].cx + (dt * Player.firespeed * Projectiles[i].xinc)
 	end
 
 
 end
+
+function projectileHitDetection()
+
+	for i = 1, #Projectiles, 1 do
+
+		for b = 1, #Arena.obstacles, 1 do
+
+			px = Projectiles[i].cx
+			py = Projectiles[i].cy
+			prad = Projectiles[i].rad
+
+			bx = Arena.obstacles[b].cx
+			by = Arena.obstacles[b].cy
+			brad = Arena.obstacles[b].rad
+
+			dx = px - bx
+			dy = py - by
+			distance = math.sqrt((dx*dx) + (dy * dy))
+
+			if(distance < (prad + brad)) then
+				Projectiles[1].hit = true
+			end
+
+		end
+	end
+end
+
+function playerHitDetection(dt)
+
+	for b = 1, #Arena.obstacles, 1 do
+
+		--Where player will be next frame
+		px = Player.xpos + (Player.xspeed * dt)
+		py = Player.ypos + (Player.yspeed * dt) 
+		prad = Player.rad
+
+		bx = Arena.obstacles[b].cx
+		by = Arena.obstacles[b].cy
+		brad = Arena.obstacles[b].rad
+
+		dx = px - bx
+		dy = py - by
+		distance = math.sqrt((dx*dx) + (dy * dy))
+
+		if(distance < (prad + brad)) then
+			return true
+		end
+
+	end
+
+	return false
+
+end
+
+
 
 
 function animatePlayer()
@@ -513,6 +531,121 @@ function animatePlayer()
 	end
 
 end
+
+
+function updateEnemies(dt)
+
+	spawnEnemy()
+
+	for i = 1, #EnemyTable.Enemies, 1 do
+
+		--Treat as triangle
+		xlen = ((EnemyTable.Enemies[i].x - Player.xpos)) -- X (Adjacent) 
+		ylen = ((EnemyTable.Enemies[i].y - Player.ypos)) -- Y (Opposite)
+
+		r = math.sqrt((xlen * xlen) + (ylen * ylen)) -- R (Hypotenuse)
+
+		xratio = xlen / r -- Ratio of x to r (Sin[theta]), value to increase x coordinate by each dt
+		yratio = ylen / r -- Ratio of y to r (Cos[theta]), value to increase y coordinate by each dt
+
+		EnemyTable.Enemies[i].xinc = xratio
+		EnemyTable.Enemies[i].yinc = yratio		
+		
+		EnemyTable.Enemies[i].x = EnemyTable.Enemies[i].x - (EnemyTable.Enemies[i].movespeed * EnemyTable.Enemies[i].xinc * dt)
+		EnemyTable.Enemies[i].y = EnemyTable.Enemies[i].y - (EnemyTable.Enemies[i].movespeed * EnemyTable.Enemies[i].yinc * dt)
+
+		if(EnemyTable.Enemies[i].xinc > EnemyTable.Enemies[i].yinc) then 
+			if(EnemyTable.Enemies[i].yinc < 0) then
+				EnemyTable.Enemies[i].currentsprites = EnemyTable.Enemies[i].upsprites
+				EnemyTable.Enemies[i].flip = 1
+			else
+				EnemyTable.Enemies[i].currentsprites = EnemyTable.Enemies[i].downsprites
+				EnemyTable.Enemies[i].flip = 1
+			end
+		else
+			if(EnemyTable.Enemies[i].xinc < 0) then
+				EnemyTable.Enemies[i].currentsprites = EnemyTable.Enemies[i].leftsprites
+				EnemyTable.Enemies[i].flip = -1
+			else
+				EnemyTable.Enemies[i].currentsprites = EnemyTable.Enemies[i].rightsprites
+				EnemyTable.Enemies[i].flip = 1
+			end
+		end
+	end
+
+	animateEnemies()
+
+end
+
+
+function animateEnemies()
+
+
+	for i = 1, #EnemyTable.Enemies, 1 do
+
+		if(EnemyTable.Enemies[i].spriteindex == #EnemyTable.Enemies[i].currentsprites) then
+			EnemyTable.Enemies[i].spriteindex = 1
+		else
+			EnemyTable.Enemies[i].spriteindex = EnemyTable.Enemies[i].spriteindex + 1
+		end
+
+
+	end
+end
+
+function drawEnemies()
+
+	for i = 1, #EnemyTable.Enemies, 1 do
+
+		index =  EnemyTable.Enemies[i].spriteindex
+
+		love.graphics.draw(spritesheet, EnemyTable.Enemies[i].currentsprites[index], EnemyTable.Enemies[i].x, EnemyTable.Enemies[i].y)
+		fps = love.timer.getFPS( )
+		love.graphics.print(fps, 300, 300)
+	end
+
+
+end
+
+
+function spawnEnemy()
+
+	if(#EnemyTable.Enemies < EnemyTable.maxEnemies) then
+
+		xpos = math.random(16, 768);
+		ypos = math.random(16, 608);
+		enemytype = math.random()
+
+		if(enemytype < 0.5) then
+			table.insert(EnemyTable.Enemies, {x = xpos, y = ypos, 
+				upsprites = enemyType1.upsprites, downsprites = enemyType1.downsprites, 
+				leftsprites = enemyType1.leftsprites,  rightsprites = enemyType1.rightsprites, 
+				currentsprites = enemyType1.downsprites,
+				health = enemyType1.health,
+				movespeed = enemyType1.movespeed,
+				targetx = Player.x, targety = Player.y,
+				xinc = 0, yinc = 0, flip = 1, spriteindex = 1})
+		else
+				table.insert(EnemyTable.Enemies, {x = xpos, y = ypos, 
+				rad = 8, cx = xpos + 8, cy = ypos + 8,	
+				upsprites = enemyType2.upsprites, downsprites = enemyType2.downsprites, 
+				leftsprites = enemyType2.leftsprites,  rightsprites = enemyType2.rightsprites, 
+				currentsprites = enemyType2.downsprites,
+				health = enemyType2.health,
+				movespeed = enemyType2.movespeed,
+				targetx = Player.x, targety = Player.y,
+				xinc = 0, yinc = 0, flip = 1, spriteindex = 1})
+		end
+
+
+
+
+	end
+
+end
+
+
+
 
 function cleanupProjectiles()
 
@@ -539,7 +672,9 @@ function cleanupProjectiles()
 			passedy = true
 		end
 
-		if(passedx and passedy) then
+
+
+		if(passedx and passedy or Projectiles[i].hit) then
 			table.insert(toremove, i)
 		end
 
@@ -548,6 +683,75 @@ function cleanupProjectiles()
 	if #toremove >= 1 then
 		for j = 1, #toremove, 1 do
 			table.remove(Projectiles, toremove[j])
+		end
+	end
+
+
+	function createBullet(x,y) 
+
+
+		if(#Projectiles < Player.maxshots) then
+
+				--Treat as triangle
+				xlen = ((x - Player.xpos)) -- X (Adjacent) 
+				ylen = ((y - Player.ypos)) -- Y (Opposite)
+
+				r = math.sqrt((xlen * xlen) + (ylen * ylen)) -- R (Hypotenuse)
+
+				xratio = xlen / r -- Ratio of x to r (Sin[theta]), value to increase x coordinate by each dt
+				yratio = ylen / r -- Ratio of y to r (Cos[theta]), value to increase y coordinate by each dt
+
+				xd = true
+				yd = true
+
+				if(xlen < 0) then
+					xd = false
+				end
+
+				if(ylen < 0) then
+					yd = false
+				end
+
+				shot = {xpos = Player.xpos - 8, ypos = Player.ypos - 8,
+						targetx = x, targety = y, 
+						sprite = Player.shotsprite[1], 
+						xinc = xratio, yinc = yratio, 
+						xdist = xd, ydist = yd,
+						hit = false, rad = 8,
+						cx = Player.xpos, cy = Player.ypos}
+
+				table.insert(Projectiles, shot)
+
+			end
+	end
+
+
+	function swapBlock(x,y)
+
+		-- Check if any blocks are selected
+		for i = 1, #Arena.obstacles, 1 do
+
+			--Four corners of block
+			blockx1 = Arena.obstacles[i].x
+			blockx2 = blockx1 + (Arena.obstacles[i].rad * 2)
+			blocky1 = Arena.obstacles[i].y
+			blocky2 = blocky1 + (Arena.obstacles[i].rad * 2)
+
+
+			if((x >= blockx1 and x <= blockx2) and (y >= blocky1 and y <= blocky2) ) then
+
+				Arena.obstacles[i].x = Player.xpos
+				Arena.obstacles[i].y = Player.ypos
+				Arena.obstacles[i].cx = Arena.obstacles[i].x + Arena.obstacles[i].rad
+				Arena.obstacles[i].cy = Arena.obstacles[i].y + Arena.obstacles[i].rad
+
+
+				Player.xpos = x - Player.rad / 2
+				Player.ypos = y - Player.rad / 2
+
+				return	
+			end
+
 		end
 	end
 
